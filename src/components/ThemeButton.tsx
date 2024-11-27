e2c11845e50b884f838d7190995108059f39c69c
Tim Stirrat <tim.stirrat@gmail.com>
2024-11-27 11:09:02 +1000

    Add system theme selection and local persistance

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,26 +1,64 @@
 import { PrimeReactContext, PrimeIcons } from "primereact/api";
 import { Button } from "primereact/button";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect, useMemo } from "react";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
-type Theme = "light" | "dark";
+type Theme = "light" | "dark" | "system";
+
+const THEME_ROTATION: Theme[] = ["dark", "light", "system"];
+const ICONS: Record<Theme, string> = {
+  dark: PrimeIcons.MOON,
+  light: PrimeIcons.SUN,
+  system: PrimeIcons.EYE,
+};
+
+const DEFAULT_THEME: Theme = "dark";
+const THEME_CSS_ID = "app-theme";
 
 export const ThemeButton: React.FC = () => {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const systemTheme = useSystemTheme();
+  const [userTheme, setUserTheme] = useLocalStorage<Theme>(
+    "user-theme",
+    undefined
+  );
+
+  const [theme, setTheme] = useState<Theme>(userTheme ?? "system");
 
   const { changeTheme } = useContext(PrimeReactContext);
 
-  const toggleTheme = () => {
-    const newTheme = theme === "dark" ? "light" : "dark";
+  const actualTheme = theme === "system" ? systemTheme : theme;
+
+  // update initial user/system theme, once only
+  useEffect(() => {
     changeTheme?.(
-      `lara-${theme}-teal`,
-      `lara-${newTheme}-teal`,
-      "app-theme",
-      () => setTheme(newTheme)
+      `lara-${DEFAULT_THEME}-teal`,
+      `lara-${actualTheme}-teal`,
+      THEME_CSS_ID
+    );
+  });
+
+  const toggleTheme = () => {
+    const prevActualTheme = theme === "system" ? systemTheme : theme;
+    const newIndex = THEME_ROTATION.indexOf(theme) + 1;
+
+    const nextTheme = THEME_ROTATION[newIndex % THEME_ROTATION.length];
+    const nextActualTheme = nextTheme === "system" ? systemTheme : nextTheme;
+
+    // TODO: figure out why changing to system theme doesnt update properly when prev and next are
+    // correct.
+    changeTheme?.(
+      `lara-${prevActualTheme}-teal`,
+      `lara-${nextActualTheme}-teal`,
+      THEME_CSS_ID,
+      () => {
+        setTheme(nextTheme);
+        setUserTheme(nextTheme);
+      }
     );
   };
   return (
     <Button
-      icon={theme === "dark" ? PrimeIcons.SUN : PrimeIcons.MOON}
+      icon={ICONS[theme]}
       rounded
       text
       onClick={toggleTheme}
@@ -29,3 +67,27 @@ export const ThemeButton: React.FC = () => {
     />
   );
 };
+
+function useSystemTheme() {
+  const isDark = useMemo(() => {
+    return (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    );
+  }, []);
+
+  const [systemTheme, setSystemTheme] = useState<Theme>(
+    isDark ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        const newTheme = event.matches ? "dark" : "light";
+        setSystemTheme(newTheme);
+      });
+  });
+
+  return systemTheme;
+}
